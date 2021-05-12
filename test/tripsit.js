222e46a6508e31a45b91643941fc10499e2c7b4a
acidicNic <nic4096@gmail.com>
2021-05-11 22:30:53 -0700

    added getInteraction() for the "/api/tripsit/getInteraction" route.

--- test/tripsit.js
+++ test/tripsit.js
@@ -1,9 +1,20 @@
-const TripsitAPI = require('../index.js');
+const tripsitAPI = require('../index.js');
 const { assert } = require('chai');
 
-
 describe("tripsit", () => {
-  it("should ...", () =>{
-    
+
+  it("should get valid drug interaction", () =>{
+    tripsitAPI.getInteraction("lsd", "alcohol")
+    .then( data => {
+      assert.property(data, 'status');
+      assert.equal(data.status, 'Low Risk & Decrease');
+
+      assert.property(data, 'interactionCategoryA');
+      assert.equal(data.interactionCategoryA, 'lsd');
+
+      assert.property(data, 'interactionCategoryB');
+      assert.equal(data.interactionCategoryB, 'alcohol');
+    });
   });
+
 });
