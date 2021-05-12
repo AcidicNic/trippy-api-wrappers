b563ccde7ed3ef1e69750eeb0755c38dab504044
acidicNic <nic4096@gmail.com>
2021-05-11 22:51:18 -0700

    added getDrug() for the "/api/tripsit/getDrug" route.

--- test/tripsit.js
+++ test/tripsit.js
@@ -1,19 +1,49 @@
 const tripsitAPI = require('../index.js');
 const { assert } = require('chai');
 
-describe("tripsit", () => {
+describe("tripsit-api", () => {
 
-  it("should get valid drug interaction", () =>{
-    tripsitAPI.getInteraction("lsd", "alcohol")
+  it("should get valid [/api/tripsit/getInteraction]", () =>{
+    return tripsitAPI.getInteraction("lsd", "mdma")
     .then( data => {
-      assert.property(data, 'status');
-      assert.equal(data.status, 'Low Risk & Decrease');
+      assert.property((data), 'status');
+      assert.equal(data.status, 'Low Risk & Synergy');
 
       assert.property(data, 'interactionCategoryA');
       assert.equal(data.interactionCategoryA, 'lsd');
 
       assert.property(data, 'interactionCategoryB');
-      assert.equal(data.interactionCategoryB, 'alcohol');
+      assert.equal(data.interactionCategoryB, 'mdma');
+    });
+  });
+
+  it("should get invalid [/api/tripsit/getInteraction]", () =>{
+    return tripsitAPI.getInteraction("notadrug", "notadrug")
+    .then( data => {
+      assert.property(data, 'err');
+      assert.equal(data.err, true);
+      assert.property(data, 'msg');
+    });
+  });
+
+  it("should get valid [/api/tripsit/getDrug]", () =>{
+    return tripsitAPI.getDrug("mdma")
+    .then( (data) => {
+      assert.property(data, 'name');
+      assert.equal(data.name, 'mdma');
+
+      assert.property(data, 'properties');
+      assert.property(data, 'aliases');
+      assert.property(data, 'categories');
+    });
+  });
+
+  it("should get invalid [/api/tripsit/getDrug]", () =>{
+    return tripsitAPI.getDrug("notadrug")
+    .then( (data) => {
+      assert.property(data, 'err');
+      assert.equal(data.err, true);
+      assert.property(data, 'msg');
     });
   });
 
